0bd2b690240e72b058caf8ff933eed97a9955749
Archita Nemalikanti <apn32@cornell.edu>
2026-02-05 12:01:35 -0500

    too many input images fro the prompt

--- api/stream.py
+++ api/stream.py
@@ -3415,8 +3415,8 @@ async def try_on_outfit():
             "https://firebasestorage.googleapis.com/v0/b/glow-55f19.firebasestorage.app/o/IMG_7470.jpg?alt=media&token=c4e73221-8d12-461f-b38a-bdd091a47a54",
             "https://firebasestorage.googleapis.com/v0/b/glow-55f19.firebasestorage.app/o/IMG_7471.PNG?alt=media&token=9233eae6-3873-480a-a28e-0bf158131174",
             "https://firebasestorage.googleapis.com/v0/b/glow-55f19.firebasestorage.app/o/Screenshot%202026-02-05%20at%2012.46.58%E2%80%AFAM.png?alt=media&token=c7001215-29ca-45b5-85b6-1f97a0a94734",
-            "https://firebasestorage.googleapis.com/v0/b/glow-55f19.firebasestorage.app/o/Screenshot%202026-02-05%20at%2012.47.18%E2%80%AFAM.png?alt=media&token=1a3a480d-49c8-4b08-8af0-c1aec6748080",
-            "https://firebasestorage.googleapis.com/v0/b/glow-55f19.firebasestorage.app/o/Screenshot%202026-02-05%20at%201.15.34%E2%80%AFAM.png?alt=media&token=94b83e28-5f26-4faf-8228-06c966a7203b"
+            #"https://firebasestorage.googleapis.com/v0/b/glow-55f19.firebasestorage.app/o/Screenshot%202026-02-05%20at%2012.47.18%E2%80%AFAM.png?alt=media&token=1a3a480d-49c8-4b08-8af0-c1aec6748080",
+            #"https://firebasestorage.googleapis.com/v0/b/glow-55f19.firebasestorage.app/o/Screenshot%202026-02-05%20at%201.15.34%E2%80%AFAM.png?alt=media&token=94b83e28-5f26-4faf-8228-06c966a7203b"
             # "https://firebasestorage.googleapis.com/v0/b/glow-55f19.firebasestorage.app/o/Screenshot%202026-02-05%20at%201.06.55%E2%80%AFAM.png?alt=media&token=293953a8-8889-4645-82a6-533ba33a00e7",
             # "https://firebasestorage.googleapis.com/v0/b/glow-55f19.firebasestorage.app/o/Screenshot%202026-02-05%20at%201.08.02%E2%80%AFAM.png?alt=media&token=19600381-b32a-466d-b223-32716a95d1f8",
             # "https://firebasestorage.googleapis.com/v0/b/glow-55f19.firebasestorage.app/o/Screenshot%202026-02-05%20at%201.08.30%E2%80%AFAM.png?alt=media&token=21a4a5e0-0ebc-4333-941d-d3b6073e4e06"
